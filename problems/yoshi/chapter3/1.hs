89896328ec5b0d0db19d557428352fcc938db01c
takumiyoshikawa <happyriver157@gmail.com>
2024-06-30 16:28:57 +0900

    feat: chapter3

---
+++ problems/yoshi/chapter3/1.hs
@@ -0,0 +1,15 @@
+a::[Char]
+a = ['a', 'b', 'c']
+
+b::(Char, Char, Char)
+b = ('a', 'b', 'c')
+
+
+c ::  [(Bool, Char)]
+c = [(False, '0'), (True, '1')]
+
+d :: ([Bool], [Char])
+d = ([False, True], ['0', '1'])
+
+e :: [[a] -> [a]]
+e = [tail, init, reverse]
